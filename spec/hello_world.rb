39d2a92b39094fe6083d5f1a5cd45a9d0bcab6c8
Kevin King <king122212@gmail.com>
2019-04-08 15:37:25 +0000

    Automatically backed up by Learn

--- spec/hello_world.rb
+++ spec/hello_world.rb
@@ -1,1 +1,11 @@
-puts "Hello World!"
+describe "Hello World" do
+  it 'has a file hello_world.rb' do
+    expect(File.exists?("hello_world.rb")).to be_truthy, 
+  end
+
+  it 'puts "Hello World!"' do
+    puts "Hello World!"
+    expect($stdout).to receive(:puts).with("Hello World!")
+    load './hello_world.rb'
+  end
+end
